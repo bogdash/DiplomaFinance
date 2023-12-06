f5ce12be27d722662c0a010c41a27719cd8f3570
Anastasia <ms.anastasia.bogdanova@gmail.com>
2023-12-06 21:00:59 +0300

    add menu and navigation

--- app/src/main/java/com/bogdash/diplomafinance/MainActivity.kt
+++ app/src/main/java/com/bogdash/diplomafinance/MainActivity.kt
@@ -2,10 +2,22 @@ package com.bogdash.diplomafinance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.bogdash.diplomafinance.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        val bottomNavigationMenu: BottomNavigationView = binding.bottomNavigationMenu
+        bottomNavigationMenu.setupWithNavController(navController)
     }
 }
