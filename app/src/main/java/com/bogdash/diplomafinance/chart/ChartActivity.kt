160a3640f8df2acff001358d71c830c6028ff42e
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-01-15 15:13:42 +0300

    add chart

--- app/src/main/java/com/bogdash/diplomafinance/chart/ChartActivity.kt
+++ app/src/main/java/com/bogdash/diplomafinance/chart/ChartActivity.kt
@@ -1,5 +1,6 @@
 package com.bogdash.diplomafinance.chart
 
+import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,9 +10,15 @@ import android.view.WindowInsetsController
 import android.widget.TextView
 import com.bogdash.diplomafinance.R
 import com.bogdash.diplomafinance.databinding.ActivityChartBinding
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.utils.ColorTemplate
 
 class ChartActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChartBinding
+    private lateinit var lineChart: LineChart
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -37,6 +44,54 @@ class ChartActivity : AppCompatActivity() {
         val toolbar = binding.topAppBar
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        chartDrawing()
+    }
+
+    private fun chartDrawing() {
+        // исходный график
+        lineChart = binding.lineChart
+        val list: ArrayList<Entry> = ArrayList()
+
+        list.add(Entry(1f, 5f))
+        list.add(Entry(2f, 3f))
+        list.add(Entry(3f, 4f))
+        list.add(Entry(4f, 2f))
+        list.add(Entry(5f, 1f))
+        list.add(Entry(6f, 6f))
+        list.add(Entry(7f, 7f))
+        list.add(Entry(8f, 8f))
+        list.add(Entry(9f, 8f))
+        list.add(Entry(10f, 5f))
+
+        // сглаженные данные
+        val movingList: ArrayList<Entry> = ArrayList()
+
+        movingList.add(Entry(3f, 4f))
+        movingList.add(Entry(4f, 3f))
+        movingList.add(Entry(5f, 2.33f))
+        movingList.add(Entry(6f, 3f))
+        movingList.add(Entry(7f, 4.66f))
+        movingList.add(Entry(8f, 7f))
+        movingList.add(Entry(9f,7.66f))
+        movingList.add(Entry(10f, 7f))
+
+        // отображение линий
+        val lineDataSet1 = LineDataSet(list, "List")
+        lineDataSet1.setColors(ColorTemplate.MATERIAL_COLORS, 255)
+        lineDataSet1.valueTextColor = Color.BLACK
+
+        val lineDataSet2 = LineDataSet(movingList, "Moving List")
+        lineDataSet2.setColors(ColorTemplate.JOYFUL_COLORS, 255)
+        lineDataSet2.valueTextColor = Color.BLACK
+        lineDataSet2.mode = LineDataSet.Mode.CUBIC_BEZIER
+
+        val lineData = LineData(lineDataSet1, lineDataSet2)
+
+        lineChart.data = lineData
+        lineChart.description.text = "Line chart"
+
+        lineChart.animateY(2000)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
