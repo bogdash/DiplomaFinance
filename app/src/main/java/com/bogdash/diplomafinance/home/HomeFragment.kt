1947cf7bb2050f04e71ac8a01a4b0a80a45753a6
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-01-22 12:17:36 +0300

    update home

--- app/src/main/java/com/bogdash/diplomafinance/home/HomeFragment.kt
+++ app/src/main/java/com/bogdash/diplomafinance/home/HomeFragment.kt
@@ -6,14 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bogdash.diplomafinance.R
+import com.bogdash.diplomafinance.databinding.FragmentHomeBinding
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment() {
-
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSignUp.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_signUpFragment)
+        }
+
+        binding.buttonLogIn.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_logInFragment)
+        }
     }
 }
