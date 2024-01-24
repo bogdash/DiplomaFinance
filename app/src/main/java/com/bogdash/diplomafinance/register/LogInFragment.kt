eaed4b3f6e03e1214cfc92b8eefc8451c7d5e23f
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-01-24 11:24:07 +0300

    add log in

---
+++ app/src/main/java/com/bogdash/diplomafinance/register/LogInFragment.kt
@@ -0,0 +1,20 @@
+package com.bogdash.diplomafinance.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bogdash.diplomafinance.R
+
+class LogInFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log_in, container, false)
+    }
+
+}
